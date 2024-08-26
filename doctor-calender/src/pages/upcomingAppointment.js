2bc0bec4deb1ac045cfa8c019f463840984895d5
Prachi Navale <navale.p@northeastern.edu>
2024-08-26 10:12:34 -0400

    Styling for upcoming appointments page

--- doctor-calender/src/pages/upcomingAppointment.js
+++ doctor-calender/src/pages/upcomingAppointment.js
@@ -1,21 +1,37 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { IoPersonOutline } from "react-icons/io5";
+import { CgCalendarDates } from "react-icons/cg";
+import { IoMdTime } from "react-icons/io";
 
 const UpcomingAppointments = () => {
   const appointments = useSelector((state) => state.appointments.appointments);
 
   return (
-    <div>
-      <h1>Upcoming Appointments</h1>
-      <ul>
+    <div className="p-10">
+      <h1 className="text-center font-semibold text text-3xl mb-10 text-blue-700">
+        Upcoming Appointments
+      </h1>
+      <div className="flex flex-wrap gap-4">
         {appointments.length > 0 ? (
           appointments.map((appointment, index) => (
-            <li key={index}>
-              <p>Patient: {appointment.patient}</p>
-              <p>Type: {appointment.appointmentType}</p>
-              <p>
+            <div
+              key={index}
+              className=" bg-white shadow-md border border-gray-200 rounded-lg p-4 w-64 flex-shrink-0"
+            >
+              <p className="font-semibold text-neutral-700 flex items-center gap-2">
+                <IoPersonOutline className="text-neutral-700" />
+                Patient: {appointment.patient}
+              </p>
+              <p className="text-sm text-gray-600">
+                Type: {appointment.appointmentType}
+              </p>
+              <p className="text-sm mt-2 text-neutral-700 flex items-center gap-2">
+                <CgCalendarDates />
                 Date: {new Date(appointment.time.start).toLocaleDateString()}
-                <br />
+              </p>
+              <p className="text-sm text-neutral-700 flex items-center gap-2">
+                <IoMdTime />
                 Time:{" "}
                 {new Date(appointment.time.start).toLocaleTimeString([], {
                   hour: "2-digit",
@@ -27,12 +43,14 @@ const UpcomingAppointments = () => {
                   minute: "2-digit",
                 })}
               </p>
-            </li>
+            </div>
           ))
         ) : (
-          <p>No upcoming appointments.</p>
+          <p className="text-xl text-neutral-700">
+            No upcoming appointments....
+          </p>
         )}
-      </ul>
+      </div>
     </div>
   );
 };
